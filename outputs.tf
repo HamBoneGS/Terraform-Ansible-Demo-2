9494424d490be377a9d98ab37c3d53cfcec0efac
HamBoneGS <59839249+HamBoneGS@users.noreply.github.com>
2020-10-13 17:39:51 -0500

    removed excess files

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-### Creates Ansible inventory file
-resource "local_file" "AnsibleInventory" {
-  content = templatefile("inventory.tmpl",
-  {
-    droplet-ip = digitalocean_droplet.www-1.ipv4_address
-    }
-    )
-  filename = "inventory"
-}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-################################################################
-# Variables file
-
-variable "ssh_user" {
-  description = "SSH user name to connect to instance."
-  default = "graham"
-}
